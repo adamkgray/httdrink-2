9dc7de42d60ddfd327f2abd5de40ac05aacecb31
agray <agray@pch.com>
2018-10-01 08:39:47 -0400

    shuffle

--- public/js/index.js
+++ public/js/index.js
@@ -76,9 +76,9 @@ const answerUI = (tag, result) => {
     switch (result) {
         case "correct":
             console.log("correct! Updating UI for correct!");
-            console.log(tag.style);
+            console.log(tag.style.backgroundColor);
             tag.style = "transition: 0.5s; background-color: #4CAF50;";
-            console.log(tag.style);
+            console.log(tag.style.backgroundColor);
             return
         case "incorrect":
             tag.style = "transition: 0.5s; background-color: #a53a37;";
