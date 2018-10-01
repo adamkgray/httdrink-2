070ff93a89d2de9c39f02682251bb82e23ba6273
agray <agray@pch.com>
2018-10-01 08:51:01 -0400

    shuffle

--- public/js/index.js
+++ public/js/index.js
@@ -73,16 +73,8 @@ graydux.setState([], {
 // HELPER FUNCTIONS
 
 const answerUI = (tag, result) => {
-    switch (result) {
-        case "correct":
-            tag.style = "background-color: #4CAF50; transition: 0.5s";
-            return
-        case "incorrect":
-            tag.style = "transition: 0.5s; background-color: #a53a37;";
-            return
-        default:
-            return
-    }
+    tag.style = "background-color: #4CAF50; transition: 0.5s";
+    //tag.style = "transition: 0.5s; background-color: #a53a37;";
 }
 
 const victoryUI = (team) => {
